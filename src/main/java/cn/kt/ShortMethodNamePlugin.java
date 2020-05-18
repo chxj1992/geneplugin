e4765d3c886f2a17bd6d819c6a854577ccff0759
chxj1992 <chxj1992@gmail.com>
2020-05-18 23:04:45 +0800

    add shortMethodNamePlugin

---
+++ src/main/java/cn/kt/ShortMethodNamePlugin.java
@@ -0,0 +1,28 @@
+package cn.kt;
+
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.PluginAdapter;
+
+import java.util.List;
+
+/**
+ * @author chenxiaojing
+ */
+public class ShortMethodNamePlugin extends PluginAdapter {
+
+    @Override
+    public boolean validate(List<String> list) {
+        return true;
+    }
+
+    @Override
+    public void initialized(IntrospectedTable introspectedTable) {
+
+        introspectedTable.setSelectByPrimaryKeyStatementId("selectById");
+        introspectedTable.setDeleteByPrimaryKeyStatementId("deleteById");
+        introspectedTable.setUpdateByPrimaryKeyStatementId("updateById");
+        introspectedTable.setUpdateByPrimaryKeySelectiveStatementId("updateByIdSelective");
+        introspectedTable.setUpdateByPrimaryKeyWithBLOBsStatementId("updateByIdWithBlobs");
+    }
+
+}
